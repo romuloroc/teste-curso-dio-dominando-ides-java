bc2dbfc9dc32d4180810ba39cdd85384f83e1103
Romulo Cardoso <romuloroc@yahoo.com.br>
2021-11-15 18:18:38 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+
+	}
+
+}
+
+class Livro{
+	private String nome;
+	private String npag;
+}
