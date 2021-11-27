7056528cde4ef8719582c25be3c067b4532449f1
Romulo Cardoso <romuloroc@yahoo.com.br>
2021-11-27 15:28:31 -0300

    Alterei ....

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -6,7 +6,7 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		//oi
 		int a, b;
 		
 		System.out.println("Digite o primeiro valor: ");
